afedd8764235a931932dfec33ea96a4e8b4ae4ad
Erik <git@erikbrewster.com>
2021-10-05 11:19:29 -0700

    ran black

--- setup.py
+++ setup.py
@@ -5,25 +5,24 @@ import os
 import sys
 
 try:
-	from setuptools import setup
+    from setuptools import setup
 except ImportError:
-	from distutils.core import setup
+    from distutils.core import setup
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
     sys.exit()
 
 setup(
-	name='pyping',
-	version='0.0.6',
-	description='A pure python ICMP ping implementation using raw sockets',
-	long_description=open('README.rst').read() + '\n\n' +
-                     open('HISTORY.rst').read(),
-	license=open("LICENSE").read(),
-	author="toxinu",
-	author_email="toxinu@gmail.com",
-	url='https://github.com/toxinu/pyping/',
-	keywords="ping icmp network latency",
-	packages = ['pyping'],
-	scripts=["bin/pyping"]
+    name="pyping",
+    version="0.0.6",
+    description="A pure python ICMP ping implementation using raw sockets",
+    long_description=open("README.rst").read() + "\n\n" + open("HISTORY.rst").read(),
+    license=open("LICENSE").read(),
+    author="toxinu",
+    author_email="toxinu@gmail.com",
+    url="https://github.com/toxinu/pyping/",
+    keywords="ping icmp network latency",
+    packages=["pyping"],
+    scripts=["bin/pyping"],
 )
